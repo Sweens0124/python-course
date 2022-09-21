6516c8190c6d9ddd91909598dcdf243b7b414557
Lucas Sweeney <lsweeney0124@gmail.com>
2022-09-21 16:40:26 -0600

    python course start

---
+++ example.py
@@ -0,0 +1,26 @@
+# print('hello world')
+
+# <--- numbers review --->
+
+# print(25 * 4)
+# print(25 + 25 + 25 + 25)
+
+# <--- indexing string --->
+
+# mystring = 'Hello World'
+# print(mystring[0:5])
+
+# <--- String Props and Methods ---> 
+
+# 'This is a {} string'.format('formatted')
+# output -> This is a formatted string
+
+# <--- Lists in Python ---> 
+
+# mylist = [1, 2 , 3]
+# mylist[1:] = [2, 3]
+# another_list = [4, 5]
+# mylist + another_list = [1, 2, 3, 4, 5]
+
+# <--- Dictionaries in Python --->
+
